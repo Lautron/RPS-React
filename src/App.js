c6ecfb6b3d4d6c7eb96ca376a3ea674b31661fc4
Lautaro Bachmann <laurigbach@gmail.com>
2021-05-27 08:55:14 -0300

    Finished creating the rest of the components, made the game is playable

--- src/App.js
+++ src/App.js
@@ -1,27 +1,89 @@
 import './App.css';
 import React, {useState} from "react";
+import playMove from "./gameLogic.js";
 
-let move = props => {
-    return <button onClick={props.playMove}>{props.icon}</button>
+let Move = props => {
+    return <button onClick={() => playMove(props.type, props.handler)} className={props.className} >{props.type}</button>
+}
+
+let BotMoves = props => {
+    return (
+	<div className="move-container">
+	    <Move className="move-icon-ai" type="R" />
+	    <Move className="move-icon-ai" type="P"/>
+	    <Move className="move-icon-ai" type="S"/>
+	</div>
+    )
+}
+
+let PlayerMoves = props => {
+    return (
+	<div className="move-container">
+	    <Move handler={props.handler} className="move-icon" type="rock"/>
+	    <Move handler={props.handler} className="move-icon" type="paper"/>
+	    <Move handler={props.handler} className="move-icon" type="scissors"/>
+	</div>
+    )
+}
+
+let Score = props => {
+    return (
+    <div className="col">
+	<span>{props.name}</span>
+	<span id="ai-score">{props.points}</span>
+    </div>
+    )
+}
+
+let Scoreboard = props => {
+    return (
+    <div className="scoreboard">
+	<Score name="Bot" points={props.botScore} />
+	<Score name="Player" points={props.playerScore} />
+    </div>
+    )
+}
+
+let Status = props => {
+    return <p id="move-result" className="versus">{props.result}</p>
 }
 
 let Game = () => {
-    //Status
-    //	scores
-    let [playerScore, setPlayerScore] = useState(0);
-    let [botScore, setBotScore] = useState(0);
-    //	status
+    let [Scores, setScores] = useState({
+	"bot": 0,
+	"player": 0,
+    });
     let [matchStatus, setMatchStatus] = useState("VS")
 
-    //Scoreboard
-    <Scoreboard playerPoints={playerScore} botPoints={botScore} />
+    let changeScores = (result) => {
+	if (result === "TIE") {
+	    return
+	}
 
-    //bot moves
-    <botMoves />
-    //Status
-    <Status/>
-    //Player moves
-    <playerMoves />
+	let scoresCopy = {...Scores}
+	if (result === "Lose") {
+	    scoresCopy.bot += 1
+	}
+	else {
+	    scoresCopy.player += 1
+	}
+	setScores(scoresCopy)
+    }
+    let changeState = (result) =>{
+	console.log(result)
+	setMatchStatus(result)
+	changeScores(result)
+    }
+    return (
+	<div className="container">
+	<div>
+	    <Scoreboard scores={Scores}/>
+	    <BotMoves/>
+	    <Status result={matchStatus}/>
+	    <PlayerMoves handler={changeState}/>
+	</div>
+	</div>
+    )
 }
 function App() {
   return <Game />;
